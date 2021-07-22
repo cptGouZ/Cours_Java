dbde7f001605652bc1da4065bcbdbdc5ede6b0d0
captGouZ <julien.gazeaurousseau@gmail.com>
2021-07-22 15:07:32 +0200

    Création web service et choix des dépendances

--- src/_80_WebService/ConfApi.java
+++ src/_80_WebService/ConfApi.java
@@ -1,7 +1,8 @@
 package _80_WebService;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
 
 @ApplicationPath("/api")
-public class ConfApi {
+public class ConfApi extends Application {
 }
